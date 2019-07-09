2a45482e284cdae45488c85c00d6152bcc7e64ec
Fillipe Goulart <fillipe.gsm@gmail.com>
2019-07-09 12:34:17 -0300

    Update helper

--- Octave-Matlab/dtlz_dimension_check.m
+++ Octave-Matlab/dtlz_dimension_check.m
@@ -1,14 +1,14 @@
 %DTLZ_DIMENSION_CHECK
-%  Check the dimension of an input x for a specific DTLZ function, and returns 
-%  an error if size(x, 1) is different from (M - 1) + k.
+%   Check the dimension of an input x for a specific DTLZ function, and returns 
+%   an error if size(x, 1) is different from (M - 1) + k.
 %
-%  Syntax:
-%     dtlz_dimension_check(x, M, k)
+%   Syntax:
+%      dtlz_dimension_check(x, M, k)
 %
-%  Input arguments:
-%     x: a (n x mu) matrix with mu points and dimension n
-%     M: a scalar with the number of objectives
-%     k: k parameter specific to each DTLZ function
+%   Input arguments:
+%      x: a (n x mu) matrix with mu points and dimension n
+%      M: a scalar with the number of objectives
+%      k: k parameter specific to each DTLZ function
 function dtlz_dimension_check (x, M, k)
    
    n = (M-1) + k; %required dimension   

8b56b62ea1760ff24fda3fe677f54ce897da640f
Fillipe Goulart <fillipe.gsm@gmail.com>
2019-07-10 13:33:40 -0300

    Update dtlz_distance code to remove repetition

--- Octave-Matlab/dtlz_distance.m
+++ Octave-Matlab/dtlz_distance.m
@@ -1,61 +1,46 @@
-function d = dtlz_distance(fname, xopt)
 %DTLZ_DISTANCE Finds the distance of a point to the optimum
 %   Since, in the DTLZ functions, the last k variables of a decision vector
 %   represent the "distance" to the Pareto set and the first ones the
 %   "position" in the front, a good way to find the distance to the optimum
 %   is to check these last variables.
 %   For each function, the last k variables must be a given value, like 0.5,
-%   1, 0 etc. Hence, the distance from xopt(end-k+1:end) to 0.5, 0, whatever
-%   will give how far the points are to the optimal set.
+%   1, 0 etc. Hence, the distance from xopt(end-k+1:end) to 0.5, (or 0, or 
+%   whatever) will provide a measure of how far the points are to the optimal 
+%   set.
 %
 %   Syntax:
 %      d = dtlz_distance(fname, xopt)
 %
 %   Input arguments:
-%      xopt: a n x mu matrix of mu individuals of dimension n (it must be
-%            in accordance with the required dimension given by Deb)
 %      fname: a string with the name of the function (e.g., 'dtlz1')
+%      xopt: a (n x mu) matrix of mu individuals of dimension n (it must be
+%            in accordance with the required dimension given by Deb)
 %
 %   Output argument:
 %      d: a mu-vector with the distance of each point to the optimal set
+function d = dtlz_distance(fname, xopt)
+   % For each function, a different value of k and reference point is given   
+   switch fname   
+      case 'dtlz1' %k = 5 and x(last) = 0.5
+         k = 5;
+         value = 0.5;
+         
+      case {'dtlz2', 'dtlz3', 'dtlz4', 'dtlz5'} %k = 10 and x(last) = 0.5
+         k = 10;
+         value = 0.5;
+         
+      case 'dtlz6' %k = 10 and x(last) = 0
+         k = 10;
+         value = 0;
+         
+      case 'dtlz7' %k = 20 and x(last) = 0
+         k = 20;
+         value = 0;
 
-% For each function, a different value of k and reference point is given.
-% So, I will just use a stupid switch and everybody's happy
-mu = size(xopt,2); %number of points
-switch fname
+      otherwise
+         error('Sorry, what the heck of a function is %s?', fname)
+   end
    
-   case 'dtlz1' %k = 5 and x(last) = 0.5
-      k = 5;
-      xlast = repmat(0.5, [k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-      
-   case 'dtlz2' %k = 10 and x(last) = 0.5
-      k = 10;
-      xlast = repmat(0.5, [k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-      
-   case 'dtlz3' %k = 10 and x(last) = 0.5
-      k = 10;
-      xlast = repmat(0.5, [k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-   
-   case 'dtlz4' %k = 10 and x(last) = 0.5
-      k = 10;
-      xlast = repmat(0.5, [k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-      
-   case 'dtlz5' %k = 10 and x(last) = 0.5
-      k = 10;
-      xlast = repmat(0.5, [k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-      
-   case 'dtlz6' %k = 10 and x(last) = 0
-      k = 10;
-      xlast = zeros([k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-      
-   case 'dtlz7' %k = 20 and x(last) = 0
-      k = 20;
-      xlast = zeros([k mu]);
-      d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
-end
+   mu = size(xopt,2); %number of points
+   xlast = value(ones(k, mu)); %replicate the required value
+   d = sum((xopt(end-k+1:end,:) - xlast).^2, 1);
